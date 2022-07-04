38039551a8afc4c9822b0aa4114322ecb2ef5575
Chris Harn <chris.harn@gmail.com>
2022-07-04 00:35:56 -0700

    Moved placeholder cout code in Main.cpp to Application.cpp.  Added placeholder code for two windows and a quad.

--- OpenGL-Implementation/Source/Application.cpp
+++ OpenGL-Implementation/Source/Application.cpp
@@ -1,10 +1,25 @@
+#include <iostream> // cout, endl
+
 #include "Application.h"
+
+//#include "OpenGL\Window.h"
+//#include "OpenGL\Quad.h"
+//
+//Window *g_pMainWindow;
+//Window *g_pSecondaryWindow;
+//Quad *g_pQuad;
 
 Application::Application() : AppRunning(true) {
 
 }
 
 bool Application::Initialization( unsigned window_width, unsigned int window_height, const char *title ) {
+    //g_pMainWindow = new Window();
+    //g_pSecondaryWindow = new Window();
+    //g_pQuad = new Quad();
+
+    std::cout << "Program started without issue." << std::endl;
+
     return true;
 }
 
@@ -17,11 +32,15 @@ void Application::Update() {
 }
 
 void Application::Render() {
-
+    std::cout << "Program finished one frame." << std::endl;
 }
 
 void Application::CleanUp() {
+    //if( g_pMainWindow != nullptr ) delete g_pMainWindow;
+    //if( g_pSecondaryWindow != nullptr ) delete g_pSecondaryWindow;
+    //if( g_pQuad != nullptr ) delete g_pQuad;
 
+    std::cout << "Program finished without issue." << std::endl;
 }
 
 bool Application::AppStillOpen() {
--- OpenGL-Implementation/Source/Main.cpp
+++ OpenGL-Implementation/Source/Main.cpp
@@ -1,22 +1,17 @@
-#include <iostream> // cout, endl
+
 
 #include "Application.h"
 
 int main()
 {
-    std::cout << "Program started without issue." << std::endl;
-
     Application mainApp;
     if( mainApp.Initialization( 600, 480, "Image Processing digital, VHS videos." ) ) {
         if(mainApp.AppStillOpen()) {
             mainApp.ProcessInput();
             mainApp.Update();
             mainApp.Render();
-            std::cout << "Program finished one frame." << std::endl;
         }
     }
 
     mainApp.CleanUp();
-
-    std::cout << "Program finished without issue." << std::endl;
 }
